68004860a95614bb9eb8d38a58dc706acecb6162
Mpp216 <mariappereira96@gmail.com>
2024-01-15 20:14:27 +0100

    Add AI to js to generate the poems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "My poem",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.getElementById("instructions").value;
+  let apiKey = "8b274fd4b3b0of2fe273a1bf90eetcd2";
+  let context =
+    "You are a romantic poem expert and love to write short poems to make people more happier. Your mission is generate one poem with four lines poems in basic html format. Do not use a tittle. In the end of the poem sign with `SheCode AI` inside a <strong> element.  Please follow the instructions. ";
+  let prompt = `User instructions: Generate a happy poem with ${instructionsInput}.`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  //consolo.log("generating poem");
+  console.log(`Prompt: ${prompt}`);
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#poem-generator-form");
